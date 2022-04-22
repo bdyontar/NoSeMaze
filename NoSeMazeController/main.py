c5a4b9493ef4e9daf9cede3626c0a48b5235414c
Michael Bram <michael.bram.92@gmail.com>
2022-04-22 16:08:37 +0200

    change name to nosemaze

--- NoSeMazeController/main.py
+++ NoSeMazeController/main.py
@@ -37,14 +37,50 @@ from HelperFunctions import Email as email
 
 
 class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
+    """The main window class of the UI.
+
+    Inherits QMainWindow from QtWidgest and uses UI design from
+    mainWindow.py.
+
+    Attributes
+    ----------
+    saved_status : bool
+        Status if experiments already saved. Checked while closing the windows
+        and changed if any trials are executed or experiment is saved.
+    config_path : str
+        Path to the config files.
+    hardware_prefs : dict
+        Hardware preferences.
+    dropbox_path : str
+        Path to dropbox to save deadman-switch, warning and error messages.
+    hardware_window : instance of AppWindows.HardwareWindow class
+        Hardware window to configure hardware preferences.
+    animal_window : instance of AppWindows.AnimalWindow class
+        Animal window to input animals in the experiment and their schedules.
+    analysis_window : instance of AppWindows.AnalysisWindow class
+        Analysis window that shows the performance curve of the experiment and of
+        each mouses respectively.
+    mail_window : instance of AppWindows.MailWindow class
+        Mail window to input the mailing list.
+    control_window: instance of AppWindows.ControlWindow class
+        Control window which shows video feed of usb cameras connected. Currently not maintained.
+
+    Note
+    ----
+    E-Mailing functionality is deprecated as messages are now saved in a cloud folder.
+    """
+
     saved = QtCore.pyqtSignal()
+    """QtCore.pyqtSignal: pyqtSignal sent if experiment is saved."""
+
     loaded = QtCore.pyqtSignal()
+    """QtCore.pyqtSignal: pyqtSignal sent if experiment is loaded."""
 
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
         self.saved_status = True
-        self.config_path = ".autonomouseconfig"
+        self.config_path = ".nosemazeconfig"
 
         # create config folder if not exists
         if not os.path.isdir(self.config_path):
@@ -60,7 +96,7 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         self.control_window = None
         self.setup_experiment_bindings(Experiment.Experiment())
 
-        # function bindings
+        # binding functions to the signals
         self.actionAnimal_List.triggered.connect(self.open_animal_window)
         self.actionHardware_Preferences.triggered.connect(
             self.open_hardware_window)
@@ -74,6 +110,14 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         self.saved.connect(self.experiment_saved)
 
     def setup_experiment_bindings(self, experiment):
+        """
+        Set up experiment in the GUI and populate the experiment table in the GUI.
+
+        Paremeters
+        ----------
+        experiment : instance of Experiment.Experiment class
+            Experiment to be set up.
+        """
         self.experiment = experiment
 
         if self.experiment.default_row.maxlen != Experiment.Experiment().default_row.maxlen:
@@ -112,6 +156,13 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         self.trialView.selectionModel().selectionChanged.connect(self.on_trial_selected)
 
     def load_config_data(self):
+        """Load hardware configuration data. Load files from path defined in config_path.
+
+        Returns
+        -------
+        hardware_configs : dict
+            Hardware configurations saved in hardware.config
+        """
         hardware_config_path = self.config_path+"/hardware.config"
         if os.path.exists(hardware_config_path):
             with open(hardware_config_path, 'rb') as fn:
@@ -120,6 +171,13 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             return None
 
     def load_dropbox_path(self):
+        """Load dropbox path saved in dropbox.txt to dropbox_path_file variable.
+
+        Returns
+        -------
+        dropbox_path_file : str
+            Path to dropbox file for messages to be written.
+        """
         dropbox_path_file = self.config_path+"/dropbox.txt"
         if os.path.exists(dropbox_path_file):
             with open(dropbox_path_file, 'r') as fn:
@@ -128,34 +186,38 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             return None
 
     def open_animal_window(self):
+        """Open animal window."""
         self.animal_window = AppWindows.AnimalWindow(self)
         self.animal_window.changed.connect(self.status_changed)
         self.animal_window.show()
 
     def open_hardware_window(self):
+        """Open hardware window."""
         self.hardware_window.show()
-        #hardware_window = AppWindows.HardwareWindow(self)
-        # hardware_window.show()
 
     def open_control_window(self):
+        """Open control window."""
         self.control_window = AppWindows.ControlWindow(
             self.experiment, parent=self)
         self.control_window.show()
 
     def open_mail_window(self):
+        """Open mail window."""
         self.mail_window = AppWindows.MailWindow(self)
         self.mail_window.show()
 
     def open_analysis_window(self):
+        """Open analysis window."""
         self.analysis_window = AppWindows.AnalysisWindow(
             self.experiment, parent=self)
         self.analysis_window.show()
 
     def update_trial_view(self):
+        """Update graph view of the trial if a trial is executed."""
         self.model.layoutChanged.emit()
 
-    # DONE thoraxView is obsolete. ACHTUNG: von GUI wegkommentieren!
     def update_data_view(self):
+        """Update data view of the trial."""
         self.dataLView.plotItem.clear()
         self.dataRView.plotItem.clear()
         self.dataLView.plotItem.plot(np.arange(len(self.experiment.last_data_l)) / self.hardware_prefs['samp_rate'],
@@ -166,6 +228,13 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         self.dataRView.setYRange(0, 6)
 
     def update_graphics_view(self, trial):
+        """Update graph view to the odour signal of the selected trial.
+
+        Parameters
+        ----------
+        trial : int
+            Trial index to be viewed.
+        """
         animal = self.experiment.trials[trial][0]
         sched_idx = self.experiment.trials[trial][2]
         trial_idx = self.experiment.trials[trial][3]
@@ -191,6 +260,7 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             pass
 
     def on_trial_selected(self):
+        """Update trial if trial is selected."""
         try:
             selected_trial = self.trialView.selectionModel().selectedRows()[
                 0].row()
@@ -199,15 +269,17 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
         self.update_graphics_view(selected_trial)
 
     def update_experiment_info(self):
+        """Update experiment labels in the GUI."""
         self.experimentNameLabel.setText(self.experiment.name)
         self.experimentDateLabel.setText(self.experiment.date)
         self.savePathLabel.setText(self.experiment.save_path)
 
     def save_experiment(self):
+        """Save experiment."""
         try:
             # bit messy, what if the experiment class changes, should rather be saving the data in the class
             fname, suff = QtWidgets.QFileDialog.getSaveFileName(
-                self, "Save Experiment", '', "AutonoMouse Experiment (*.autmaus)")
+                self, "Save Experiment", '', "NoSeMaze Experiment (*.nosemaze)")
             tmpName = os.path.basename(fname)
             tmpSavePath = os.path.dirname(fname)
             self.experiment.name = tmpName
@@ -242,6 +314,7 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             print('were not saved...')
 
     def set_dropbox_path(self):
+        """Set dropbox path in which the deadman-switch, warning and error messages to be saved."""
         dropbox_path = self.load_dropbox_path()
 
         try:
@@ -261,17 +334,20 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             print("were not saved...")
 
     def experiment_saved(self):
+        """Changing status and name of window if experiment is saved."""
         self.saved_status = True
-        self.setWindowTitle('AutonoMouse2')
+        self.setWindowTitle('NoSeMaze')
 
     def status_changed(self):
+        """Changing status and name of window if a trial is executed."""
         self.saved_status = False
-        self.setWindowTitle('AutonoMouse2*')
+        self.setWindowTitle('NoSeMaze*')
 
     def load_experiment(self):
+        """Load experiment."""
         try:
             fname, suff = QtWidgets.QFileDialog.getOpenFileName(
-                self, "Open Experiment", '', "AutonoMouse2 Experiment (*.autmaus)")
+                self, "Open Experiment", '', "NoSeMaze Experiment (*.nosemaze)")
 
             if fname != '':
                 with open(fname, 'rb') as fn:
@@ -284,9 +360,11 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             QtWidgets.QMessageBox.about(self, "Error", "Cannot load data")
 
     def thread_control(self):
+        """Stop experiment control thread."""
         self.experiment_control.stop()
 
     def windows_control(self):
+        """Check if any window is opened, then close it."""
         if self.animal_window is not None:
             self.animal_window.close()
         if self.analysis_window is not None:
@@ -299,6 +377,13 @@ class MainApp(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
             self.control_window.close()
 
     def closeEvent(self, event):
+        """Things to be executed if closeEvent occured (x in main window is clicked).
+        
+        Parameters
+        ----------
+        event : instance of QCloseEvent
+            Events with close event information sent to the window.
+        """
         if self.saved_status == False:
             answer = QtWidgets.QMessageBox.question(self, "Warning", "Data has not been saved.\nSave changes?", QtWidgets.QMessageBox.Yes |
                                                     QtWidgets.QMessageBox.No | QtWidgets.QMessageBox.Cancel, QtWidgets.QMessageBox.Cancel)
@@ -328,10 +413,23 @@ sys._excepthook = sys.excepthook
 
 
 def my_exception_hook(exctype, value, traceback):
+    """Custom exception hook.
+
+    Parameters
+    ----------
+    exctype : type of BaseException
+        Type of exception.
+    value : BaseException
+        Exception occured.
+    traceback : TracebackType
+        Traceback up to the exception.
+    """
     email.crash_error(exctype, value, traceback)
+    
     # Print the error and traceback
-#    test = "".join(traceback.format_exception(exctype,value,traceback))
-#    print(test)
+    # test = "".join(traceback.format_exception(exctype,value,traceback))
+    # print(test)
+    
     # Call the normal Exception hook after
     sys._excepthook(exctype, value, traceback)
     sys.exit(1)
@@ -342,6 +440,7 @@ sys.excepthook = my_exception_hook
 
 
 def main():
+    """Main method to be called."""
     app = QtWidgets.QApplication(sys.argv)
     form = MainApp()
     form.show()
